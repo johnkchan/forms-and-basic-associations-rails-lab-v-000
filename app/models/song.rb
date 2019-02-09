09721eedc5a8d9147b1cf4316d9276ccdf084779
Jonathan Chan <johnkchan252@gmail.com>
2019-02-09 22:48:52 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -13,4 +13,12 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil
   end
   
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+  
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+  
 end
